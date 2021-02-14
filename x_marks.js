0614b15287b925d8d15a9b914df0c3ceff865488
Rashad Bayram <brashad@rashadbayram.local>
2021-02-14 13:15:34 -0700

    x_marks final solution added

---
+++ x_marks.js
@@ -0,0 +1,33 @@
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']  
+let north = 1;
+let south = -1;
+let west = 1;
+let east = -1;
+
+
+const finalPosition = function (moves) {
+  // Your code in here ...
+
+  let location = [0, 0];
+
+  for(let move of moves){
+    switch(move) {
+      case 'north':
+        location[1] +=1;
+        break;
+      case 'south':
+        location[1] -=1;
+        break;
+      case 'east':
+        location[0] +=1;
+        break;
+      case 'west':
+        location[0] -=1;
+        break;
+    }
+  }
+  return location;
+}
+
+console.log(finalPosition(moves));
+
