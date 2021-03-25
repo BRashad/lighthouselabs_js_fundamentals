67644ba395afa2e6f36bb9f0409ecb86da662b70
Rashad <rashadabayram@gmail.com>
2021-03-24 23:25:11 -0600

    final katas

--- Kata_21/script.js
+++ Kata_21/script.js
@@ -1,33 +1,35 @@
-let prompt = require("prompt-sync")();
-//accumulating attempts qty
-//let guessedNum = [];
+let answer;
+let guessedNum = [];
 let attempts = 0;
 const randomNum = Math.floor(Math.random() * 10) + 1; 
-let answer = Number(prompt("Guess a number: "));
 
-const generator = () => {
   while (answer !== randomNum) { //loop runs while answer is not equal random number
-
+    let prompt = require("prompt-sync")();
+    answer = Number(prompt("Guess a number: "));
       if(!answer) {
-        return console.log("No number")
+        return console.log("No number. Please enter a number")
       } else if (answer === randomNum) {
-        attempts++;// does not accumulate 
-        console.log('You gussed it right!');
+        guessedNum.push(answer);
+        console.log('You guessed it right!');
         
       } else if (answer < randomNum) {
-        attempts++;
+        guessedNum.push(answer);
         console.log('Too small');
         
       } else if (answer > randomNum) {
-        attempts++;
-        console.log('Too large');
-        
+        guessedNum.push(answer);
+        console.log('Too large'); 
       }
     }
+
+  for(i = 0; i < guessedNum.length; i++){
+    //console.log('Guessed number array:' + guessedNum);
+    if(guessedNum.indexOf(guessedNum[i]) === guessedNum.lastIndexOf(guessedNum[i])){
+      attempts++;
+    } 
   }
 
 
-generator();
 console.log(randomNum);
 console.log(attempts);
 console.log("You answered: " + answer);
@@ -35,7 +37,7 @@ console.log("You answered: " + answer);
 
 /*
  - After every guess the program tells the user whether their number was too large or too small. DONE
- - At the end, the number of tries needed should be printed.
+ - At the end, the number of tries needed should be printed. Done
  - Inputting the same number multiple times should only count as one try. 
  - If the user provides an answer which isn't a number, print an error message and do not count this as a try.
 */
--- kata_20.js
+++ kata_20.js
@@ -1,16 +1,66 @@
 const blocksAway = function(arr) {
   // Put your solution here
-let output = {east: 0, north: 0};
+let output = {
+  east: 0, 
+  north: 0
+};
 const left = 'left';
 const right = 'right';
- 
-for (let i = 0; i < arr.length; i += 2) {
+let currDir;
+
+if(arr[0] === left){
+  currDir = 'north';
+  output.north = arr[1];
+} else if(arr[0] === right) {
+  currDir = 'east';
+  output.east = arr[1];
   
- if(arr[i] === right)  output.east += arr[i + 1];
- if(arr[i] === left)  output.north += arr[i + 1];
+}
+
+for (i = 2; i <= arr.length - 2; i += 2) {
+
+  if(currDir === 'north'  && arr[i] === right) {
+    currDir = "east";
+    output.east += arr[i+1];
+    continue;
+  } else if(currDir === 'north'  && arr[i] === left) {
+      currDir = "west";
+      output.east -= arr[i+1];
+      continue;
+  } 
+  
+  if (currDir === 'west' && arr[i] === right) {
+    currDir = "south";
+    output.north -= arr[i+1];
+    continue;
+  } else if (currDir === 'west' && arr[i] === left ) {
+    currDir = "north";
+    output.north += arr[i+1];
+    continue;
+  }
+
+  if (currDir === 'east' && arr[i] === right) {
+    currDir = "south";
+    output.north -= arr[i+1];
+    continue;
+  } else if (currDir === 'east' && arr[i] === left ) {
+    currDir = "north";
+    output.north += arr[i+1];
+    continue;
+  }
+
+  if (currDir === 'south' && arr[i] === right) {
+    currDir = "west";
+    output.east -= arr[i+1];
+    continue;
+  } else if (currDir === 'south' && arr[i] === left ) {
+    currDir = "east";
+    output.east += arr[i+1];
+
+  }
 
 }
-  
+
   return output;
 };
 
